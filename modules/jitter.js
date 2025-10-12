8363a41c6341732e156f517ab9c7347ba8bca4bc
Jaume Sanchez <the.spite@gmail.com>
2025-10-12 19:09:18 +0100

    Create jitter.js

---
+++ modules/jitter.js
@@ -0,0 +1,128 @@
+import { PerspectiveCamera, MathUtils, Matrix4 } from "three";
+
+const jitterTable = [
+  [0.5625, 0.4375],
+  [0.0625, 0.9375],
+  [0.3125, 0.6875],
+  [0.6875, 0.8124],
+  [0.8125, 0.1875],
+  [0.9375, 0.5625],
+  [0.4375, 0.0625],
+  [0.1875, 0.3125],
+];
+let jitterPointer = 0;
+
+function makePerspectiveJitter(
+  mat,
+  left,
+  right,
+  top,
+  bottom,
+  near,
+  far,
+  offsetX,
+  offsetY,
+  w,
+  h
+) {
+  if (far === undefined) {
+    console.warn(
+      "THREE.Matrix4: .makePerspective() has been redefined and has a new signature. Please check the docs."
+    );
+  }
+
+  const scaleX = (left - right) / w;
+  const scaleY = (top - bottom) / h;
+
+  left -= offsetX * scaleX;
+  top -= offsetY * scaleY;
+  right -= offsetX * scaleX;
+  bottom -= offsetY * scaleY;
+
+  var te = mat.elements;
+  var x = (2 * near) / (right - left);
+  var y = (2 * near) / (top - bottom);
+
+  var a = (right + left) / (right - left);
+  var b = (top + bottom) / (top - bottom);
+  var c = -(far + near) / (far - near);
+  var d = (-2 * far * near) / (far - near);
+
+  te[0] = x;
+  te[4] = 0;
+  te[8] = a;
+  te[12] = 0;
+  te[1] = 0;
+  te[5] = y;
+  te[9] = b;
+  te[13] = 0;
+  te[2] = 0;
+  te[6] = 0;
+  te[10] = c;
+  te[14] = d;
+  te[3] = 0;
+  te[7] = 0;
+  te[11] = -1;
+  te[15] = 0;
+
+  return mat;
+}
+
+function updateProjectionMatrixJitter(camera, size) {
+  const [offsetX, offsetY] = jitterTable[jitterPointer];
+
+  var near = camera.near,
+    top = (near * Math.tan(MathUtils.DEG2RAD * 0.5 * camera.fov)) / camera.zoom,
+    height = 2 * top,
+    width = camera.aspect * height,
+    left = -0.5 * width,
+    view = camera.view;
+
+  if (camera.view !== null && camera.view.enabled) {
+    var fullWidth = view.fullWidth,
+      fullHeight = view.fullHeight;
+
+    left += (view.offsetX * width) / fullWidth;
+    top -= (view.offsetY * height) / fullHeight;
+    width *= view.width / fullWidth;
+    height *= view.height / fullHeight;
+  }
+
+  var skew = camera.filmOffset;
+  if (skew !== 0 && camera.getFilmWidth)
+    left += (near * skew) / camera?.getFilmWidth();
+
+  if (camera instanceof PerspectiveCamera) {
+    const mat = makePerspectiveJitter(
+      camera.projectionMatrix,
+      left,
+      left + width,
+      top,
+      top - height,
+      near,
+      camera.far,
+      offsetX,
+      offsetY,
+      size.x,
+      size.y
+    );
+  } else {
+    camera.left -= offsetX / size.x;
+    camera.top -= offsetY / size.y;
+    camera.right -= offsetX / size.x;
+    camera.bottom -= offsetY / size.y;
+    camera.updateProjectionMatrix();
+  }
+
+  camera.projectionMatrixInverse.copy(camera.projectionMatrix).invert();
+}
+
+function incPointer() {
+  jitterPointer = (jitterPointer + 1) % jitterTable.length;
+}
+
+function resetPointer() {
+  jitterPointer = 0;
+}
+
+export { updateProjectionMatrixJitter, incPointer, resetPointer };
