964dab34a4af1676e1efed1d5f71310cbbfee3ac
Jaume Sanchez <the.spite@gmail.com>
2025-10-12 16:43:54 +0100

    Update three-meshline.js

--- modules/three-meshline.js
+++ modules/three-meshline.js
@@ -384,87 +384,86 @@ MeshLine.prototype.advance = function (position) {
   this._attributes.next.needsUpdate = true;
 };
 
-ShaderChunk["meshline_vert"] = [
-  "",
-  ShaderChunk.logdepthbuf_pars_vertex,
-  ShaderChunk.fog_pars_vertex,
-  "",
-  "attribute vec3 previous;",
-  "attribute vec3 next;",
-  "attribute float side;",
-  "attribute float width;",
-  "attribute float counters;",
-  "",
-  "uniform vec2 resolution;",
-  "uniform float lineWidth;",
-  "uniform vec3 color;",
-  "uniform float opacity;",
-  "uniform float sizeAttenuation;",
-  "",
-  "varying vec2 vUV;",
-  "varying vec4 vColor;",
-  "varying float vCounters;",
-  "",
-  "vec2 fix( vec4 i, float aspect ) {",
-  "",
-  "    vec2 res = i.xy / i.w;",
-  "    res.x *= aspect;",
-  "	 vCounters = counters;",
-  "    return res;",
-  "",
-  "}",
-  "",
-  "void main() {",
-  "",
-  "    float aspect = resolution.x / resolution.y;",
-  "",
-  "    vColor = vec4( color, opacity );",
-  "    vUV = uv;",
-  "",
-  "    mat4 m = projectionMatrix * modelViewMatrix;",
-  "    vec4 finalPosition = m * vec4( position, 1.0 );",
-  "    vec4 prevPos = m * vec4( previous, 1.0 );",
-  "    vec4 nextPos = m * vec4( next, 1.0 );",
-  "",
-  "    vec2 currentP = fix( finalPosition, aspect );",
-  "    vec2 prevP = fix( prevPos, aspect );",
-  "    vec2 nextP = fix( nextPos, aspect );",
-  "",
-  "    float w = lineWidth * width;",
-  "",
-  "    vec2 dir;",
-  "    if( nextP == currentP ) dir = normalize( currentP - prevP );",
-  "    else if( prevP == currentP ) dir = normalize( nextP - currentP );",
-  "    else {",
-  "        vec2 dir1 = normalize( currentP - prevP );",
-  "        vec2 dir2 = normalize( nextP - currentP );",
-  "        dir = normalize( dir1 + dir2 );",
-  "",
-  "        vec2 perp = vec2( -dir1.y, dir1.x );",
-  "        vec2 miter = vec2( -dir.y, dir.x );",
-  "        //w = clamp( w / dot( miter, perp ), 0., 4. * lineWidth * width );",
-  "",
-  "    }",
-  "",
-  "    //vec2 normal = ( cross( vec3( dir, 0. ), vec3( 0., 0., 1. ) ) ).xy;",
-  "    vec4 normal = vec4( -dir.y, dir.x, 0., 1. );",
-  "    normal.xy *= .5 * w;",
-  "    normal *= projectionMatrix;",
-  "    if( sizeAttenuation == 0. ) {",
-  "        normal.xy *= finalPosition.w;",
-  "        normal.xy /= ( vec4( resolution, 0., 1. ) * projectionMatrix ).xy;",
-  "    }",
-  "",
-  "    finalPosition.xy += normal.xy * side;",
-  "",
-  "    gl_Position = finalPosition;",
-  "",
-  ShaderChunk.logdepthbuf_vertex,
-  ShaderChunk.fog_vertex &&
-    "    vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );",
-  ShaderChunk.fog_vertex,
-  "}",
-].join("\n");
+ShaderChunk["meshline_vert"] = `
+  ${ShaderChunk.logdepthbuf_pars_vertex}
+  ${ShaderChunk.fog_pars_vertex}
+  
+  attribute vec3 previous;
+  attribute vec3 next;
+  attribute float side;
+  attribute float width;
+  attribute float counters;
+  
+  uniform vec2 resolution;
+  uniform float lineWidth;
+  uniform vec3 color;
+  uniform float opacity;
+  uniform float sizeAttenuation;
+  
+  varying vec2 vUV;
+  varying vec4 vColor;
+  varying float vCounters;
+  
+  vec2 fix( vec4 i, float aspect ) {
+  
+      vec2 res = i.xy / i.w;
+      res.x *= aspect;
+  	 vCounters = counters;
+      return res;
+  
+  }
+  
+  void main() {
+  
+      float aspect = resolution.x / resolution.y;
+  
+      vColor = vec4( color, opacity );
+      vUV = uv;
+  
+      mat4 m = projectionMatrix * modelViewMatrix;
+      vec4 finalPosition = m * vec4( position, 1.0 );
+      vec4 prevPos = m * vec4( previous, 1.0 );
+      vec4 nextPos = m * vec4( next, 1.0 );
+  
+      vec2 currentP = fix( finalPosition, aspect );
+      vec2 prevP = fix( prevPos, aspect );
+      vec2 nextP = fix( nextPos, aspect );
+  
+      float w = lineWidth * width;
+  
+      vec2 dir;
+      if( nextP == currentP ) dir = normalize( currentP - prevP );
+      else if( prevP == currentP ) dir = normalize( nextP - currentP );
+      else {
+          vec2 dir1 = normalize( currentP - prevP );
+          vec2 dir2 = normalize( nextP - currentP );
+          dir = normalize( dir1 + dir2 );
+  
+          vec2 perp = vec2( -dir1.y, dir1.x );
+          vec2 miter = vec2( -dir.y, dir.x );
+          //w = clamp( w / dot( miter, perp ), 0., 4. * lineWidth * width );
+  
+      }
+  
+      //vec2 normal = ( cross( vec3( dir, 0. ), vec3( 0., 0., 1. ) ) ).xy;
+      vec4 normal = vec4( -dir.y, dir.x, 0., 1. );
+      normal.xy *= .5 * w;
+      normal *= projectionMatrix;
+      if( sizeAttenuation == 0. ) {
+          normal.xy *= finalPosition.w;
+          normal.xy /= ( vec4( resolution, 0., 1. ) * projectionMatrix ).xy;
+      }
+  
+      finalPosition.xy += normal.xy * side;
+  
+      gl_Position = finalPosition;
+  
+  ${ShaderChunk.logdepthbuf_vertex}
+  ${ShaderChunk.fog_vertex}
+  vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+  ${ShaderChunk.fog_vertex}
+  }
+`;
 
 ShaderChunk["meshline_frag"] = `
   ${ShaderChunk.fog_pars_fragment}
@@ -475,12 +474,13 @@ ShaderChunk["meshline_frag"] = `
   uniform float useMap;
   uniform float useAlphaMap;
   uniform float useDash;
-  uniform float dashArray;
+  uniform vec2 dashArray;
   uniform float dashOffset;
   uniform float dashRatio;
   uniform float visibility;
   uniform float alphaTest;
   uniform vec2 repeat;
+  uniform vec2 uvOffset;
   uniform sampler2D blueNoiseMap;
   uniform vec2 resolution;
   uniform float offset;
@@ -510,11 +510,21 @@ ShaderChunk["meshline_frag"] = `
     ${ShaderChunk.logdepthbuf_fragment}
   
     vec4 c = vColor;
-    vec4 t = texture( map, vUV * repeat );
 
-    t.a *= opacity;
+    vec2 tuv = (vUV + uvOffset) * repeat;
+    if(useDash == 1.) {
+      tuv.x = mod((tuv.x + dashOffset),1.);
+    }
 
-    if(t.a < .01) {
+    vec4 t = texture( map, tuv );
+
+    float alpha = t.r * opacity *t.a;
+
+    if( useDash == 1. ){
+      alpha *= ceil(mod(vCounters*repeat.x+dashOffset,1.) - (dashArray.x / (dashArray.x+dashArray.y)));
+    }
+    
+    if(alpha < .01) {
       discard;
     }
 
@@ -523,12 +533,11 @@ ShaderChunk["meshline_frag"] = `
     uv = rot2d(uv, time);
     uv += offset;
 
-    if(blueNoise(uv) > t.a) {
+    if(blueNoise(uv) > alpha) {
       discard;
     }
-    if( useDash == 1. ){
-        c.a *= ceil(mod(vCounters + dashOffset, dashArray) - (dashArray * dashRatio));
-    }
+    
+    c.a = 1.;
     gl_FragColor = c;
 
     ${ShaderChunk.fog_fragment}
@@ -557,11 +566,13 @@ class MeshLineMaterial extends ShaderMaterial {
         dashOffset: { value: 0 },
         offset: { value: 0 },
         dashRatio: { value: 0.5 },
+        dashArray: { value: new Vector2(1, 1) },
         useDash: { value: 0 },
         visibility: { value: 1 },
         alphaTest: { value: 0 },
         time: { value: 0 },
         repeat: { value: new Vector2(1, 1) },
+        uvOffset: { value: new Vector2(0, 0) },
       }),
 
       vertexShader: ShaderChunk.meshline_vert,
@@ -726,6 +737,15 @@ class MeshLineMaterial extends ShaderMaterial {
           this.uniforms.repeat.value.copy(value);
         },
       },
+      uvOffset: {
+        enumerable: true,
+        get: function () {
+          return this.uniforms.uvOffset.value;
+        },
+        set: function (value) {
+          this.uniforms.uvOffset.value.copy(value);
+        },
+      },
       time: {
         enumerable: true,
         get: function () {
@@ -768,6 +788,7 @@ MeshLineMaterial.prototype.copy = function (source) {
   this.visibility = source.visibility;
   this.alphaTest = source.alphaTest;
   this.repeat.copy(source.repeat);
+  this.uvOffset.copy(source.uvOffset);
 
   return this;
 };
