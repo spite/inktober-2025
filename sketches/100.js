26c634da879a7698af2abdebcee7b07a36db0590
Jaume Sanchez <the.spite@gmail.com>
2025-11-10 00:55:41 +0000

    Update 100.js

--- sketches/100.js
+++ sketches/100.js
@@ -22,61 +22,155 @@ import {
   Line,
   Raycaster,
 } from "three";
-import { renderer, getCamera, isRunning, onResize } from "../modules/three.js";
-import { MeshLine, MeshLineMaterial } from "../modules/three-meshline.js";
+import { renderer, getCamera, onResize } from "../modules/three.js";
 import Maf from "maf";
 import { palette2 as palette } from "../modules/floriandelooij.js";
 import { gradientLinear } from "../modules/gradient.js";
 import { OrbitControls } from "OrbitControls";
-import { Easings } from "../modules/easings.js";
-import { Painted } from "../modules/painted.js";
 import { pointsOnSphere } from "../modules/points-sphere.js";
-import { curl, seedFunc, generateNoiseFunction } from "../modules/curl.js";
-import { RoundedCylinderGeometry } from "../modules/rounded-cylinder-geometry.js";
-import { march, sdIcosahedron } from "../modules/raymarch.js";
-import { Poisson2D } from "../modules/poisson-2d.js";
+import { seedFunc, generateNoiseFunction } from "../modules/curl.js";
 import { init } from "../modules/dipoles-3d.js";
 import { CustomTubeGeometry } from "../modules/CustomTubeGeometry.js";
 import { GLTFExporter } from "../third_party/GLTFExporter.js";
+import {
+  computeBoundsTree,
+  disposeBoundsTree,
+  acceleratedRaycast,
+} from "../third_party/bvh.js";
+import { MeshSurfaceSampler } from "../third_party/MeshSurfaceSampler.js";
+import { loadStanfordBunny, loadSuzanne } from "../modules/models.js";
+
+// Add the extension functions
+BufferGeometry.prototype.computeBoundsTree = computeBoundsTree;
+BufferGeometry.prototype.disposeBoundsTree = disposeBoundsTree;
+Mesh.prototype.raycast = acceleratedRaycast;
+
 const group = new Group();
 
 const WIDTH = 1;
 const HEIGHT = 1;
 const DEPTH = 1;
-const points = pointsOnSphere(2000);
-const NUMCHARGES = 20;
-const chargePoints = pointsOnSphere(NUMCHARGES);
-const charges = init(NUMCHARGES, WIDTH, HEIGHT, DEPTH, 0.01);
-const tmp = new Vector3();
-charges.charges.forEach((p, i) => {
-  const pc = chargePoints[i];
-  tmp.set(pc.x, pc.y, pc.z);
-  // tmp.set(
-  //   Maf.randomInRange(-1, 1),
-  //   Maf.randomInRange(-1, 1),
-  //   Maf.randomInRange(-1, 1)
-  // );
-  // tmp.set(p.x, p.y, p.z);
-  tmp.normalize();
-  p.x = tmp.x;
-  p.y = tmp.y;
-  p.z = tmp.z;
+const NUMCHARGES = 50;
+const POINTS = 2000;
 
-  // const mesh = new Mesh(
-  //   new IcosahedronGeometry(0.1, 2),
-  //   new MeshNormalMaterial()
-  // );
-  // mesh.position.copy(tmp);
-  // group.add(mesh);
-});
+let obj = {};
+let points = [];
+let charges = [];
 
-const painted = new Painted({ minLevel: -0.2 });
-// const curl = generateNoiseFunction();
+async function initPoints() {
+  // obj = initSphere();
+  // obj = await initSuzanne();
+  obj = await initStanfordBunnt();
 
-onResize((w, h) => {
-  const dPR = renderer.getPixelRatio();
-  painted.setSize(w * dPR, h * dPR);
-});
+  renderLines();
+}
+
+function initSphere() {
+  const r = 0.25;
+  points = pointsOnSphere(POINTS, r);
+  const chargePoints = pointsOnSphere(NUMCHARGES);
+  const mesh = new Mesh(new IcosahedronGeometry(r, 7), material);
+  group.add(mesh);
+
+  charges = init(NUMCHARGES, WIDTH, HEIGHT, DEPTH, 0.01);
+  const tmp = new Vector3();
+  charges.charges.forEach((p, i) => {
+    const pc = chargePoints[i];
+    tmp.set(pc.x, pc.y, pc.z);
+    tmp.normalize().multiplyScalar(r);
+    p.x = tmp.x;
+    p.y = tmp.y;
+    p.z = tmp.z;
+  });
+
+  return {
+    fn: (v) => v.normalize(),
+    step: (v) => {
+      const dir = charges.calcDirection(v.x, v.y, v.z);
+      tmp.set(dir.x, dir.y, dir.z).clampLength(0, 0.002);
+      v.add(tmp);
+      v.normalize().multiplyScalar(r);
+    },
+  };
+}
+
+async function initSuzanne() {
+  const geo = await loadSuzanne();
+  geo.scale(0.25, 0.25, 0.25);
+  geo.computeBoundsTree();
+
+  const mesh = new Mesh(geo, material);
+
+  group.add(mesh);
+
+  const sampler = new MeshSurfaceSampler(mesh).build();
+  const position = new Vector3();
+
+  for (let i = 0; i < POINTS; i++) {
+    sampler.sample(position);
+    points.push(position.clone());
+  }
+
+  charges = init(50, 1, 1, 1, 1);
+  charges.charges.forEach((p, i) => {
+    sampler.sample(position);
+    p.x = position.x;
+    p.y = position.y;
+    p.z = position.z;
+    p.charge = Maf.randomInRange(-100, 100);
+  });
+
+  const tmp = new Vector3();
+  return {
+    step: (v) => {
+      const dir = charges.calcDirection(v.x, v.y, v.z);
+      tmp.copy(dir).clampLength(0, 0.002);
+      v.add(tmp);
+      geo.boundsTree.closestPointToPoint(v, tmp);
+      v.copy(tmp.point);
+    },
+  };
+}
+
+async function initStanfordBunnt() {
+  const geo = await loadStanfordBunny();
+  geo.scale(2, 2, 2);
+  geo.computeBoundsTree();
+
+  const mesh = new Mesh(geo, material);
+
+  group.add(mesh);
+
+  const sampler = new MeshSurfaceSampler(mesh).build();
+  const position = new Vector3();
+
+  for (let i = 0; i < POINTS; i++) {
+    sampler.sample(position);
+    points.push(position.clone());
+  }
+
+  charges = init(50, 1, 1, 1, 1);
+  charges.charges.forEach((p, i) => {
+    sampler.sample(position);
+    p.x = position.x;
+    p.y = position.y;
+    p.z = position.z;
+    p.charge = Maf.randomInRange(-100, 100);
+  });
+
+  const tmp = new Vector3();
+  return {
+    step: (v) => {
+      const dir = charges.calcDirection(v.x, v.y, v.z);
+      tmp.copy(dir).clampLength(0, 0.002);
+      v.add(tmp);
+      geo.boundsTree.closestPointToPoint(v, tmp);
+      v.copy(tmp.point);
+    },
+  };
+}
+
+const curl = generateNoiseFunction();
 
 palette.range = [
   "#026BFA",
@@ -107,10 +201,6 @@ const camera = getCamera();
 const scene = new Scene();
 const controls = new OrbitControls(camera, canvas);
 controls.screenSpacePanning = true;
-controls.addEventListener("change", () => {
-  painted.invalidate();
-});
-painted.backgroundColor.set(new Color(0xf6f2e9));
 
 camera.position
   .set(-0.38997204674241887, -0.1646326072361011, 0.3548472598819808)
@@ -121,86 +211,71 @@ renderer.setClearColor(0xf8fcfe, 1);
 
 const strokeTexture = new TextureLoader().load("./assets/brush4.jpg");
 strokeTexture.wrapS = strokeTexture.wrapT = RepeatWrapping;
-const resolution = new Vector2(canvas.width, canvas.height);
 
 const meshes = [];
 
-const material = new MeshNormalMaterial(); //MeshBasicMaterial({ wireframe: true, color: 0 });
-const sphere = new Mesh(new IcosahedronGeometry(1, 7), material);
-group.add(sphere);
+const material = new MeshNormalMaterial({ wireframe: !true });
+// const material = new MeshBasicMaterial(); //{ wireframe: true, color: 0 });
 
 function renderLines() {
-  const step = 1;
+  const SEGMENTS = 200; //Math.round(Maf.randomInRange(50, 100));
   const v = new Vector3();
+  const curves = [];
+  let minLength = Number.MAX_SAFE_INTEGER;
+  let maxLength = Number.MIN_SAFE_INTEGER;
+
   for (const pt of points) {
-    v.set(pt.x, pt.y, pt.z).normalize();
-    let m = 5;
-    let d = { x: 0, y: 0, z: 0 };
-    let a;
+    v.set(pt.x, pt.y, pt.z);
+
+    // const m = new Mesh(
+    //   new IcosahedronGeometry(0.001, 3),
+    //   new MeshBasicMaterial({ color: 0xb70000 })
+    // );
+    // m.position.copy(v);
+    // group.add(m);
 
     const colors = [];
-    const POINTS = 200; //Math.round(Maf.randomInRange(50, 100));
 
-    const geo = new Float32Array(POINTS * 3);
-    let ptr = 0;
-    const s = 0.01;
     const pts = [];
     const t = new Vector3();
 
-    for (let j = 0; j < POINTS; j++) {
-      const dir = charges.calcDirection(v.x, v.y, v.z);
-      t.set(dir.x, dir.y, dir.z).normalize().multiplyScalar(s);
+    for (let j = 0; j < SEGMENTS; j++) {
+      obj.step(v);
 
-      v.add(t);
-      v.normalize();
-
-      t.copy(v);
-      // t.multiplyScalar(1 + (j * 0.1) / POINTS);
-      geo[ptr] = t.x;
-      geo[ptr + 1] = t.y;
-      geo[ptr + 2] = t.z;
-
-      pts.push(t.clone());
+      pts.push(v.clone());
 
       const col = j / 100;
       colors.push(col, col, col);
-
-      ptr += 3;
     }
 
     const curve = new CatmullRomCurve3(pts);
     const l = curve.getLength();
+    minLength = Math.min(minLength, l);
+    maxLength = Math.max(maxLength, l);
 
+    curves.push(curve);
+  }
+
+  const range = maxLength - minLength;
+  for (const curve of curves) {
     const tubeGeometry = new CustomTubeGeometry(
       curve,
-      POINTS / 2,
-      0.02 * l,
+      SEGMENTS / 2,
+      Maf.map(
+        Math.exp(0),
+        Math.exp(1 * 2),
+        0.001,
+        0.004,
+        Math.exp((2 * (curve.getLength() - minLength)) / range)
+      ),
       36,
       false,
       (p) => Maf.parabola(p, 1)
     );
     const tube = new Mesh(tubeGeometry, material);
     group.add(tube);
-
-    // var g = new MeshLine();
-    // g.setPoints(geo, (p) => Maf.parabola(p, 0.4));
-
-    // var mesh = new Mesh(g.geometry, material);
-    // mesh.geo = geo;
-    // mesh.g = g;
-
-    // meshes.push({
-    //   mesh,
-    //   offset: Maf.randomInRange(-100, 100),
-    //   speed: Maf.randomInRange(1, 2),
-    // });
-    // group.add(mesh);
-    // lineGeometry.setAttribute("color", new Float32BufferAttribute(colors, 3));
-    // const line = new Line(lineGeometry, material);
-    // group.add(line);
   }
 }
-group.scale.set(0.25, 0.25, 0.25);
 
 const link = document.createElement("a");
 link.style.display = "none";
@@ -232,7 +307,6 @@ function exportGLTF(input) {
         saveArrayBuffer(result, "scene.glb");
       } else {
         const output = JSON.stringify(result, null, 2);
-        console.log(output);
         saveString(output, "scene.gltf");
       }
     },
@@ -251,32 +325,14 @@ button.addEventListener("click", (e) => {
 });
 document.body.querySelector(".recordPanel").append(button);
 
-renderLines();
 scene.add(group);
-
-let lastTime = performance.now();
-let time = 0;
 
 function draw(startTime) {
   const t = performance.now();
 
-  if (isRunning) {
-    time += (t - lastTime) / 20000;
-    painted.invalidate();
-  }
-
-  meshes.forEach((m) => {
-    m.mesh.material.uniforms.uvOffset.value.x = -(
-      time * 10 * m.speed +
-      m.offset
-    );
-  });
-
-  group.rotation.y = time * Maf.TAU;
-
   renderer.render(scene, camera);
-  // painted.render(renderer, scene, camera);
-  lastTime = t;
 }
+
+initPoints();
 
 export { draw, canvas, renderer, camera };
