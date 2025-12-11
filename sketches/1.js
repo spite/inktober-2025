5141614232b0b0497b80c48c9fbbe03b81228919
Jaume Sanchez <the.spite@gmail.com>
2025-12-11 13:40:30 +0000

    Update 1.js

--- sketches/1.js
+++ sketches/1.js
@@ -3,21 +3,59 @@ import {
   Mesh,
   Group,
   BufferGeometry,
-  Vector2,
   Vector3,
   BufferAttribute,
   TextureLoader,
   Color,
 } from "three";
-import { renderer, getCamera, isRunning, onResize } from "../modules/three.js";
+import {
+  renderer,
+  getCamera,
+  isRunning,
+  onResize,
+  onRandomize,
+} from "../modules/three.js";
 import { MeshLine, MeshLineMaterial } from "../modules/three-meshline.js";
 import Maf from "maf";
 import { palette2 as palette } from "../modules/floriandelooij.js";
 import { gradientLinear } from "../modules/gradient.js";
 import { OrbitControls } from "OrbitControls";
 import { Painted } from "../modules/painted.js";
+import GUI from "../modules/gui.js";
+import { signal, effectRAF } from "../modules/reactive.js";
 
-const painted = new Painted({ minLevel: -0.5 });
+const params = {
+  rings: signal(72),
+  ringLength: signal(1),
+  segments: signal(100),
+  tilt: signal(0.1),
+  spread: signal(0.1),
+  lineWidthMin: signal(0.3),
+  lineWidthMax: signal(0.5),
+  seed: signal(performance.now()),
+};
+
+const gui = new GUI("Annular sphere");
+gui.addLabel(
+  "Lines generated at different heights on the surface of a sphere."
+);
+gui.addSlider("Rings", params.rings, 1, 200, 1);
+gui.addSlider("Ring length", params.ringLength, 0.1, 2, 0.01);
+gui.addSlider("Segments", params.segments, 20, 100, 1);
+gui.addSlider("Tilt", params.tilt, 0, 0.2, 0.01);
+gui.addSlider("Spread", params.spread, 0, 0.2, 0.01);
+gui.addRangeSlider(
+  "Width range",
+  params.lineWidthMin,
+  params.lineWidthMax,
+  0.1,
+  0.9,
+  0.01
+);
+// gui.addSelect("Palette", ["Red", "Blue"]);
+gui.addButton("Randomize params", randomize);
+
+const painted = new Painted();
 
 onResize((w, h) => {
   const dPR = renderer.getPixelRatio();
@@ -81,6 +119,19 @@ palette.range = [
   "#F9C163",
 ];
 
+palette.range = [
+  "#1e242c",
+  "#4a5b6b",
+  "#8da0b4",
+  "#cdd9e6",
+  "#f5f8fb",
+  // "#3a8beb",
+  // "#6b9dd8",
+  // "#3ab485",
+  "#ebb43a",
+  "#e74c3c",
+];
+
 const gradient = new gradientLinear(palette.range);
 
 const canvas = renderer.domElement;
@@ -88,6 +139,7 @@ const camera = getCamera();
 const scene = new Scene();
 const group = new Group();
 const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 controls.addEventListener("change", () => {
   painted.invalidate();
@@ -98,74 +150,121 @@ camera.lookAt(group.position);
 renderer.setClearColor(0, 0);
 painted.backgroundColor.set(new Color(0xf6f2e9));
 
-const circleRadius = 2;
-const geometry = new BufferGeometry();
-const vertices = [];
-const SEGMENTS = 100;
-for (let j = 0; j <= 1 * Math.PI; j += Math.PI / SEGMENTS) {
-  const v = new Vector3(
-    0,
-    circleRadius * Math.cos(j),
-    circleRadius * Math.sin(j)
-  );
-  vertices.push(v.x, v.y, v.z);
-}
-vertices.reverse();
-geometry.setAttribute(
-  "position",
-  new BufferAttribute(new Float32Array(vertices), 3)
-);
-
-const resolution = new Vector2(canvas.width, canvas.height);
 const strokeTexture = new TextureLoader().load("./assets/brush4.jpg");
 
 const circles = [];
-const SIDES = 72;
-for (let i = 0; i < SIDES; i++) {
-  const line = new MeshLine();
-  const material = new MeshLineMaterial({
-    map: strokeTexture,
-    useMap: true,
-    color: new Color().setHSL(i / SIDES, 1, 0.5),
-    lineWidth: Maf.randomInRange(0.3, 0.5),
-    resolution: resolution,
-    offset: Maf.randomInRange(-100, 100),
-    opacity: Maf.randomInRange(0.7, 0.9),
-  });
-  line.setGeometry(geometry, (p) => p);
-  const mesh = new Mesh(line.geometry, material);
-  const pivot = new Group();
-  const a = Maf.randomInRange(0, Maf.TAU);
-  const x = 3 * Math.sin(a);
-  const y = Maf.randomInRange(-2, 2);
-  const z = 3 * Math.cos(a);
-  pivot.position.set(0, y, 0);
-  const tilt = 0.1;
-  pivot.rotation.x = Maf.randomInRange(-tilt, tilt);
-  pivot.rotation.z = Maf.randomInRange(-tilt, tilt);
-  mesh.rotation.x = Math.PI / 2;
-  pivot.add(mesh);
-  group.add(pivot);
-  mesh.scale.setScalar(
-    Maf.parabola((y + 2) / 4, 0.5) + Maf.randomInRange(-0.1, 0.1)
+const geometry = new BufferGeometry();
+
+function generateRing() {
+  console.log("Generating ring");
+  const circleRadius = 2;
+  const vertices = [];
+  const l = params.ringLength() * Math.PI;
+  for (let j = 0; j <= l; j += l / params.segments()) {
+    const v = new Vector3(
+      0,
+      circleRadius * Math.cos(j),
+      circleRadius * Math.sin(j)
+    );
+    vertices.push(v.x, v.y, v.z);
+  }
+  vertices.reverse();
+  geometry.setAttribute(
+    "position",
+    new BufferAttribute(new Float32Array(vertices), 3)
   );
-  material.lineWidth *= Maf.parabola((y + 2) / 4, 0.5);
-  circles.push({
-    mesh,
-    pivot,
-    x,
-    c: Maf.randomInRange(0, 1),
-    speed: 1 + Math.round(Maf.randomInRange(0, 2)),
-    z,
-    a,
-  });
 }
+
+function generateLines() {
+  console.log("Generating lines");
+  Math.seedrandom(params.seed());
+
+  for (let i = 0; i < params.rings(); i++) {
+    const line = new MeshLine();
+    const material = new MeshLineMaterial({
+      map: strokeTexture,
+      useMap: true,
+      color: gradient.getAt(Maf.randomInRange(0, 1)),
+      lineWidth: Maf.randomInRange(
+        params.lineWidthMin(),
+        params.lineWidthMax()
+      ),
+      offset: Maf.randomInRange(-100, 100),
+      opacity: Maf.randomInRange(0.7, 0.9),
+    });
+    line.setGeometry(geometry, (p) => p);
+    const mesh = new Mesh(line.geometry, material);
+    // Tilt the ring.
+    const pivot = new Group();
+    const a = Maf.randomInRange(0, Maf.TAU);
+    const x = 3 * Math.sin(a);
+    const y = Maf.map(0, params.rings(), -2, 2, i); //Maf.randomInRange(-2, 2);
+    const z = 3 * Math.cos(a);
+    pivot.position.set(0, y, 0);
+    // const tilt = 0.1;
+    pivot.rotation.x = Maf.randomInRange(-params.tilt(), params.tilt());
+    pivot.rotation.z = Maf.randomInRange(-params.tilt(), params.tilt());
+    mesh.rotation.x = Math.PI / 2;
+    pivot.add(mesh);
+    group.add(pivot);
+    // Adjust size to shape as sphere.
+    mesh.scale.setScalar(
+      Maf.parabola((y + 2) / 4, 0.5) +
+        Maf.randomInRange(-params.spread(), params.spread())
+    );
+    material.lineWidth *= Maf.parabola((y + 2) / 4, 0.5);
+    circles.push({
+      mesh,
+      pivot,
+      x,
+      speed: 1 + Math.round(Maf.randomInRange(0, 2)),
+      z,
+      a,
+    });
+  }
+  painted.invalidate();
+}
+
 scene.add(group);
+
+generateRing();
+generateLines();
+
+effectRAF(() => {
+  console.log("effectRAF2");
+  clearScene();
+  generateRing();
+  generateLines();
+});
+
+function clearScene() {
+  for (const mesh of circles) {
+    group.remove(mesh.pivot);
+  }
+  circles.length = 0;
+}
+
+onRandomize(() => {
+  params.seed.set(performance.now());
+});
+
+function randomize() {
+  console.log("randomize");
+  params.rings.set(Maf.intRandomInRange(1, 200));
+  params.segments.set(Maf.intRandomInRange(20, 100));
+  params.tilt.set(Maf.randomInRange(0, 0.2));
+  params.spread.set(Maf.randomInRange(0, 0.2));
+  params.ringLength.set(Maf.randomInRange(0.1, 2));
+  params.lineWidthMin.set(Maf.randomInRange(0.1, 0.9));
+  params.lineWidthMax.set(Maf.randomInRange(params.lineWidthMin(), 0.9));
+}
 
 let lastTime = performance.now();
 let time = 0;
 
-function draw(startTime) {
+function draw() {
+  controls.update();
+
   const t = performance.now();
   if (isRunning) {
     time += (t - lastTime) / 1000 / 10;
@@ -174,7 +273,6 @@ function draw(startTime) {
 
   circles.forEach((c, id) => {
     c.pivot.rotation.y = -c.speed * time * Maf.TAU + c.a;
-    c.mesh.material.uniforms.color.value.copy(gradient.getAt(c.c));
   });
 
   group.rotation.x = Maf.PI / 8;
