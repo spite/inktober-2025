92e6a73bb1a76d1a61c0b59f7f051716411eb4dc
Jaume Sanchez <the.spite@gmail.com>
2025-12-15 22:55:52 +0000

    Update 4.js

--- sketches/4.js
+++ sketches/4.js
@@ -126,7 +126,7 @@ controls.addEventListener("change", () => {
   painted.invalidate();
 });
 
-camera.position.set(5, -2.5, -16);
+camera.position.set(5, -2.5, -16).multiplyScalar(0.8);
 camera.lookAt(group.position);
 renderer.setClearColor(0, 0);
 painted.backgroundColor.set(new Color(0xf6f2e9));
