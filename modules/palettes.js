92373f26c581ae273dc54235236686f5a5dd2c5f
Jaume Sanchez <the.spite@gmail.com>
2025-12-15 01:26:39 +0000

    Create palettes.js

---
+++ modules/palettes.js
@@ -0,0 +1,54 @@
+const basic = [
+  "#1e242c",
+  "#4a5b6b",
+  "#8da0b4",
+  "#cdd9e6",
+  "#f5f8fb",
+  "#6b9dd8",
+  "#ebb43a",
+  "#e74c3c",
+];
+
+const fire = [
+  "#FD7555",
+  "#FE4F2E",
+  "#040720",
+  "#EB9786",
+  "#E02211",
+  "#3A0724",
+  "#F9C163",
+];
+
+const florian = ["#20a0aa", "#ec4039", "#ffae12"];
+const florian2 = [
+  "#612e8d",
+  "#c22286",
+  "#ea225e",
+  "#ed5b35",
+  "#f5b52e",
+  "#81c540",
+  "#00a396",
+  "#1674bc",
+];
+
+const acid_cool = ["#ffffff", "#4477aa", "#3eb9d6", "#ffcb5c"];
+
+const acid_trip = ["#ff7b00", "#ff00ff", "#ffffff"];
+
+const test = ["#ffffff", "#4477aa", "#3eb9d6", "#ffcb5c"];
+
+const grayscale = ["#000000", "#ffffff"];
+
+const palettes = {
+  basic,
+  fire,
+  florian,
+  florian2,
+  acid_cool,
+  acid_trip,
+  test,
+  grayscale,
+};
+const paletteOptions = Object.keys(palettes);
+
+export { palettes, paletteOptions };
