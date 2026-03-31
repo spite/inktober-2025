13a02f98361f99e01c07b64bdc87ff9580564f8f
Jaume Sanchez <the.spite@gmail.com>
2026-04-01 01:58:15 +0200

    Update 27.js

--- sketches/27.js
+++ sketches/27.js
@@ -297,7 +297,6 @@ scene.add(group);
 let abortController = new AbortController();
 
 const sketchEffect = effectRAF(() => {
-  console.log("effectRAF2");
   abortController.abort();
   clearScene();
   abortController = new AbortController();
@@ -318,7 +317,6 @@ function clearScene() {
 
 function randomize() {
   params.seed.set(performance.now());
-  console.log(params.seed());
 }
 
 function randomizeParams() {
