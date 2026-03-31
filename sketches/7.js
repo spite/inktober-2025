b0799d5430b82b740e67ababe954dc6bfb50b4c7
Jaume Sanchez <the.spite@gmail.com>
2026-04-01 01:56:23 +0200

    Update 7.js

--- sketches/7.js
+++ sketches/7.js
@@ -175,7 +175,6 @@ group.scale.setScalar(0.1);
 scene.add(group);
 
 const sketchEffect = effectRAF(() => {
-  console.log("effectRAF2");
   clearScene();
   generateShape();
 });
@@ -194,7 +193,6 @@ function randomize() {
 }
 
 function randomizeParams() {
-  console.log("randomize");
   params.lines.set(Maf.intRandomInRange(100, 400));
   params.loops.set(Maf.intRandomInRange(1, 10));
   // params.segments.set(Maf.intRandomInRange(200, 500));
